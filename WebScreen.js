5f748348b5dcd10e420a7ad4a10612fa8c1dfce4
dodanghoa <88731584+dodanghoa@users.noreply.github.com>
2021-08-31 10:35:48 +0700

    Comment

--- WebScreen.js
+++ WebScreen.js
@@ -7,43 +7,59 @@ import WebView from 'react-native-webview';
 import { URL_adress } from './address_config';
 import { AntDesign } from '@expo/vector-icons';
 import OptionsMenu from "react-native-options-menu";
+//device Height & device Width
 const deviceHeight = Dimensions.get('window').height;
 const deviceWidth = Dimensions.get('window').width;
+//Icon options of top menu
 const myIcon = (<SimpleLineIcons name="options-vertical" size={19} color="black" />)
 class WebScreen extends Component {
+  //WebVeiw properties to Reload
   webview = null;
+  //constructor
   constructor(props) {
     super(props);
     this.state = { link: URL_adress }
   }
-  
+  //Called immediately after a component is mounted. Setting state here will trigger re-rendering.
   componentDidMount() {
     this.setState({ link: URL_adress })
     this.Loading();
   }
-  Loading =()=>{
+  //Load the Header of WebScreen: Back button,title,options
+  Loading = () => {
     this.props.navigation.setOptions({
+      //set the tile with uri of website and slice it to fit the title
       title: <Text style={{ alignItems: 'center', fontSize: 13 }}>{this.state.link.length > 41 ? this.state.link.slice(0, 41) + "...." : this.state.link}</Text>,
       headerTitleAlign: "center",
+      //Button back
       headerLeft: () => (
         <Button icon={<AntDesign name="close" size={24} color="black" />}
           onPress={() => { this.props.navigation.goBack(); }} />
       ),
+      //Button option
       headerRight: () => (
+        //using Option menu
         <OptionsMenu
+          //set icon
           customButton={myIcon}
           destructiveIndex={1}
-          options={["Sao chép liên kết", "Mở với trình duyệt", "Làm mới","Ẩn"]}
-          actions={[this.copyToClipboard, this.OpenBrower,this.Refesh, () => { }]} />
+          //set display content
+          options={["Sao chép liên kết", "Mở với trình duyệt", "Làm mới", "Ẩn"]}
+          //set function corresponding to display content
+          actions={[this.copyToClipboard, this.OpenBrower, this.Refesh, () => { }]} />
       ),
     })
   }
-  Refesh =()=>{
+
+  //Refesh function
+  Refesh = () => {
     this.webview.reload();
   }
+  //OpenBrower function 
   OpenBrower = () => {
     Linking.openURL(this.state.link);
   }
+  //copyToClipboard function
   copyToClipboard = () => {
     Clipboard.setString(this.state.link);
     ToastAndroid.showWithGravity(
@@ -56,19 +72,24 @@ class WebScreen extends Component {
   render() {
     return (
       <SafeAreaView style={styles.container}>
-        <WebView style={{
-          width: deviceWidth,
-          height: deviceHeight
-        }} source={{ uri: URL_adress }}
+
+        <WebView 
+        //Set style with deviceWidth and deviceHeight
+        style={{width: deviceWidth, height: deviceHeight}} 
+        //Set soure with URL_adress in address_config.js
+        source={{ uri: URL_adress }}
+        //Set WebVeiw properties
         ref={ref => (this.webview = ref)}
+        //Function that is invoked when the WebView is loading. to get Current URL
         onLoadProgress={({ nativeEvent }) => {
-          this.state.link = nativeEvent.url;
-          console.log(this.state.link)
-          this.Loading()
-        }}
+            //get Current URL and reload the title 
+            this.state.link = nativeEvent.url;
+            console.log(this.state.link)
+            this.Loading()
+          }}
         >
         </WebView>
-        
+
       </SafeAreaView>
     );
   }
